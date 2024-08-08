3e6e5b813a19cfed942a1d56b90417439ac1aac2
Lucas-GBP <lucas.bitencourte.pedroso@gmail.com>
2024-08-08 01:11:44 -0300

    refactor and roll20 cell size updated

--- src/main.c
+++ src/main.c
@@ -1,20 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h" 
 
-// Math Constansts
-const double sqrt_three = 1.732050807568877293527446341505872366942805253810380628055806;
-// Roll 20 magical numbers
-const double roll20_factor = 1.028197616008545; // Roll20 height distortion factor
-const double roll20_size = 2; // [unit]
-const double roll20_hex_width = 75.1979759663; // [pixel]
-const double roll20_hex_scale_px = roll20_hex_width/(sqrt_three*roll20_size); // Total hexagon width in [pixel/unit]
-const double roll20_stroke_width = 0.05; // [unit]
-// SVG's elements
-const char* header = "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" viewBox=\"0 0 %f %f\" width=\"%fpx\" height=\"%fpx\">\n";
-const char* close_svg = "</svg>\n";
-const char* empty_hexagon = "\t<defs>\n\t\t<path id=\"H\" d=\"M%f %fl%f %fl%f %fl%f %fl%f %fl%f %fZ\" fill=\"none\" stroke=\"%s\" stroke-width=\"%.2f\"/>\n\t</defs>\n";
-const char* empty_rectangle = "\t<rect x=\"0\" y=\"0\" width=\"%f\" height=\"%f\" fill=\"%s\"/>\n";
-const char* use_hexagon = "\t\t<use xlink:href=\"#H\" x=\"%f\" y=\"%f\"/>\n";
+//#define STANDART_DATA
+//#define STANDART_ROLL20_DATA
+#define ROLL20_DATA
+
+/*
+ * Standart colors
+*/
+char* transparent = "none";
+char* black = "black";
+char* white = "white";
 
 int main(int argc, char** argv){
     // Validating inputs
@@ -24,59 +19,144 @@ int main(int argc, char** argv){
     }
 
     // Inputs
-    const long width = atol(argv[1]);
-    const long height = atol(argv[2]);
-    const int size = 2;
-    const double stroke_width =  roll20_stroke_width;
-    const char* stroke_color = "black";
-    const char* background_color = "none";
-    const double scale_px = roll20_hex_scale_px;
-    // Hexagon Blueprint
-    const double l_sqrt3_per2 = (double)(sqrt_three*size/2);
-    const double l_per2 = (double)(size/2);
-    // Calculated constants
-    const double width_scale = (double)(sqrt_three*size);
-    const double width_odd_offset = l_sqrt3_per2;
-    const double height_scale = (double)(3*l_per2);
-    const double view_box_width = (double)(width*width_scale+width_odd_offset+stroke_width/2);
-    const double view_box_height = (double)((size*(height*3+1)+stroke_width)/2)*roll20_factor;
+    #ifdef STANDART_DATA
+    population_data data = {
+        .width = atol(argv[1]),
+        .height = atol(argv[2]),
+        .size = 2,
+        .stroke_width = 0.2,
+        .stroke_color = black,
+        .background_color = transparent,
+        .scale_px = 35
+    };
+    #endif
+    #ifdef STANDART_ROLL20_DATA
+    population_data data = {
+        .width = (long)(atol(argv[1])),
+        .height = (long)(atol(argv[2])),
+        .size = roll20_standart_size,
+        .stroke_width = roll20_standart_stroke_width,
+        .stroke_color = black,
+        .background_color = transparent,
+        .scale_px = roll20_hex_width/(sqrt_three*roll20_standart_size)
+    };
+    #endif
+    #ifdef ROLL20_DATA
+    population_data data = {
+        .width = (long)(atol(argv[1])),
+        .height = (long)(atol(argv[2])*roll20_height_scale),
+        .size = roll20_standart_size*roll20_width_scale,
+        .stroke_width = roll20_standart_stroke_width,
+        .stroke_color = black,
+        .background_color = transparent,
+        .scale_px = roll20_hex_width/(sqrt_three*roll20_standart_size)
+    };
+    #endif
 
-    // Creating file
     FILE* file = fopen("file.svg", "w+");
     if(!file){
-        printf("Error:\nfopen() returned NULL");
+        printf("Error:\n\tfopen() returned NULL");
         return 1;
     }
+    populate_roll20_hex_grid(file, &data);
 
-    /*
-    * Populating file
-    */
-    //Header
+    fclose(file);
+    return 0;
+}
+
+void populate_hex_grid(FILE* file, population_data* data){
+    // Hexagon Blueprint
+    const double l_sqrt3_per2 = (double)(sqrt_three*data->size/2);
+    const double l_per2 = (double)(data->size/2);
+    // Calculated constants
+    const double width_scale = (double)(sqrt_three*data->size);
+    const double width_odd_offset = l_sqrt3_per2;
+    const double height_scale = (double)(3*l_per2);
+    const double view_box_width = (double)(data->width*width_scale+width_odd_offset+data->stroke_width/2);
+    const double view_box_height = (double)((data->size*(data->height*3+1)+data->stroke_width)/2);
+
     fprintf(file, 
         header, 
         view_box_width, view_box_height,
-        view_box_width*scale_px, view_box_height*scale_px
+        view_box_width*data->scale_px, view_box_height*data->scale_px
+    );
+    // Hexagon
+    fprintf(
+        file, empty_hexagon,
+        l_sqrt3_per2, (double)(0),
+        l_sqrt3_per2, l_per2,
+        (double)(0), (double)data->size,
+        (double)(-1*l_sqrt3_per2), l_per2,
+        (double)(-1*l_sqrt3_per2), (double)(-1*l_per2),
+        (double)(0), (double)(-1*data->size),
+        data->stroke_color, data->stroke_width
+    );
+    // Rectangle
+    fprintf(file, empty_rectangle,
+        view_box_width, view_box_height, data->background_color
+    );
+
+    fputs("\t<g>\n", file);
+    for(long i = 0; i < data->height; i++){
+        if(i%2){
+            for(long j = 0; j < data->width; j++){
+                fprintf(file, 
+                    use_hexagon, 
+                    (double)(j*width_scale+width_odd_offset), 
+                    (double)(i*height_scale)
+                );
+            }
+        } else {
+            for(long j = 0; j < data->width; j++){
+                fprintf(file, 
+                    use_hexagon, 
+                    (double)(j*width_scale), 
+                    (double)(i*height_scale)
+                );
+            }
+        }
+        fputc('\n', file);
+    }
+    fputs("\t</g>\n", file);
+    fputs(close_svg, file);
+};
+
+void populate_roll20_hex_grid(FILE* file, population_data* data){
+    // Hexagon Blueprint
+    const double l_sqrt3_per2 = (double)(sqrt_three*data->size/2);
+    const double l_per2 = (double)(data->size/2);
+    // Calculated constants
+    const double width_scale = (double)(sqrt_three*data->size);
+    const double width_odd_offset = l_sqrt3_per2;
+    const double height_scale = (double)(3*l_per2);
+    const double view_box_width = (double)(data->width*width_scale+width_odd_offset+data->stroke_width/2);
+    const double view_box_height = (double)((data->size*(data->height*3+1)+data->stroke_width)/2)*roll20_factor;
+
+    fprintf(file, 
+        header, 
+        view_box_width, view_box_height,
+        view_box_width*data->scale_px, view_box_height*data->scale_px
     );
     // Hexagon
     fprintf(
         file, empty_hexagon,
         l_sqrt3_per2, (double)(0)*roll20_factor,
         l_sqrt3_per2, l_per2*roll20_factor,
-        (double)(0), (double)size*roll20_factor,
+        (double)(0), (double)data->size*roll20_factor,
         (double)(-1*l_sqrt3_per2), l_per2*roll20_factor,
         (double)(-1*l_sqrt3_per2), (double)(-1*l_per2)*roll20_factor,
-        (double)(0), (double)(-1*size)*roll20_factor,
-        stroke_color, stroke_width*roll20_factor
+        (double)(0), (double)(-1*data->size)*roll20_factor,
+        data->stroke_color, data->stroke_width*roll20_factor
     );
     // Rectangle
     fprintf(file, empty_rectangle,
-        view_box_width, view_box_height, background_color
+        view_box_width, view_box_height, data->background_color
     );
 
     fputs("\t<g>\n", file);
-    for(long i = 0; i < height; i++){
+    for(long i = 0; i < data->height; i++){
         if(i%2){
-            for(long j = 0; j < width; j++){
+            for(long j = 0; j < data->width; j++){
                 fprintf(file, 
                     use_hexagon, 
                     (double)(j*width_scale+width_odd_offset), 
@@ -84,7 +164,7 @@ int main(int argc, char** argv){
                 );
             }
         } else {
-            for(long j = 0; j < width; j++){
+            for(long j = 0; j < data->width; j++){
                 fprintf(file, 
                     use_hexagon, 
                     (double)(j*width_scale), 
@@ -95,9 +175,5 @@ int main(int argc, char** argv){
         fputc('\n', file);
     }
     fputs("\t</g>\n", file);
-
     fputs(close_svg, file);
-    fclose(file);
-
-    return 0;
 }
---
+++ src/main.h
@@ -0,0 +1,53 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Math Constansts
+ */
+const double sqrt_three = 1.732050807568877293527446341505872366942805253810380628055806;
+const double sqrt_tree_2 = sqrt_three/2.0;
+
+/*
+ * Roll 20 magical numbers
+ */
+const double roll20_standart_cell_size = 70.0;      // [pixel]
+const double roll20_standart_stroke_width = 0.05;   // [unit]
+const double roll20_standart_size = 2.0;            // [unit]
+const double roll20_width_scale = 0.926;            // Roll20 width cell quantity ratio
+const double roll20_height_scale = 1.12;            // Roll20 height cell quantity ratio
+const double roll20_hex_width = 75.1979759663;      // [pixel]
+const double roll20_factor = 1.028197616008545;     // Roll20 height distortion factor
+
+/*
+ * SVG's elements
+ */
+const char* header = "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" viewBox=\"0 0 %f %f\" width=\"%fpx\" height=\"%fpx\">\n";
+const char* close_svg = "</svg>\n";
+const char* empty_hexagon = "\t<defs>\n\t\t<path id=\"H\" d=\"M%f %fl%f %fl%f %fl%f %fl%f %fl%f %fZ\" fill=\"none\" stroke=\"%s\" stroke-width=\"%.2f\"/>\n\t</defs>\n";
+const char* empty_rectangle = "\t<rect x=\"0\" y=\"0\" width=\"%f\" height=\"%f\" fill=\"%s\"/>\n";
+const char* use_hexagon = "\t\t<use xlink:href=\"#H\" x=\"%f\" y=\"%f\"/>\n";
+
+/*
+ * Structs
+ */
+struct population_data{
+    long width;             // Quantity of cells
+    long height;            // Quantity of cells
+    double size;            // Width of the cell [unit]
+    double stroke_width;    // Stroke width [unit]
+    char* stroke_color;     // Hex-code in string of stroke color
+    char* background_color; // Hex-code in string of background color
+    double scale_px;        // Total hexagon width in [pixel/unit]
+};
+typedef struct population_data population_data;
+
+/*
+ * Functions
+ */
+void populate_hex_grid(FILE* file, population_data* data);
+void populate_roll20_hex_grid(FILE* file, population_data* data);
+
+#endif
